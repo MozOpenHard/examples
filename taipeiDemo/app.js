c8ca1cf26761cc06f744df47e51845575ad1cae0
naokisekiguchi <na-sekiguchi@kddilabs.jp>
2015-05-29 15:56:28 +0800

    taipeidemo: add read distance sensor

--- taipeiDemo/app.js
+++ taipeiDemo/app.js
@@ -4,7 +4,8 @@ window.addEventListener("load", function() {
   Promise.all([
     navigator.setGpioPort(198,"in"),
     navigator.setGpioPort(199,"out"),
-    navigator.setI2cPort(0,0x48)
+    navigator.setI2cPort(0,0x48),
+    navigator.setI2cPort(2,0x70)
   ]).then(gpioLoad);
 });
 
@@ -17,6 +18,7 @@ function gpioLoad(ports){
   var btnPort = ports[0];
   var ledPort = ports[1];
   var tempSensor = ports[2];
+  var distSensor = ports[3];
  
   //btnEle.onclick = changeDisplayColor;
   btnEle.onclick = function(){
@@ -29,22 +31,32 @@ function gpioLoad(ports){
       toggleLed();
     }
   };
-  
+  /*
   setInterval(function(){
     readTempSensor(31.0).then(
       function(){displayColor=false;changeDisplayColor();},
       function(){displayColor=true;changeDisplayColor();}
     );
   },1000);
+  */
+  
+  setInterval(function(){
+    readDistSensor().then(function(value){
+      print("valueEle1",value);
+      if(value < 50){
+        changeDisplayColor();
+      }
+    });
+  },1000);
   
   
   function changeDisplayColor(){
     console.log("change display color.");
     if(!displayColor){
-      document.body.style.background = "#FFFFFF";
+      document.body.style.background = "#FA8072";
       displayColor = true;
     }else{
-      document.body.style.background = "#666666";
+      document.body.style.background = "#FFFFFF";
       displayColor = false;
     }
   }
@@ -75,12 +87,44 @@ function gpioLoad(ports){
         var temp = ((v[0] << 8) + v[1]) / 128.0;
         console.log(temp);
         console.log(threshold);
-        if(threshold < temp){
-          resolve();
-        }else{
-          reject();
-        }
+        resolve(temp);
+      },function(){
+        reject();
       });
     });
+  }
+  
+  function readDistSensor(threshold){
+    console.log("read dist");
+    return new Promise(function(resolve,reject){
+      distSensor.write8(0x00,0x00);
+      sleep(1);
+      distSensor.write8(0x00,0x51);
+      sleep(70);
+      Promise.all([
+        distSensor.read(0x02,true),
+        distSensor.read(0x03,true)
+      ]).then(function(v){
+        console.log(v[0]);
+        console.log(v[1]);
+        var dist = ((v[0] << 8) + v[1]);
+        resolve(dist);
+      },function(){
+        reject();
+      });
+    });
+  }
+  
+  
+  //utils
+  function print(eleName,str){
+    var ele = document.getElementById(eleName);
+    ele.innerHTML = str;
+    console.log(str);
+  }
+  
+  function sleep(millisec) {
+    var start = new Date();
+    while(new Date() - start < millisec);
   }
 }
